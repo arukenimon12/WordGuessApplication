729caf002ec1bef9b053ba439b7f6de2e4bd5cfb
Michael John Revilla <revilla.261443@gensan.sti.edu.ph>
2024-04-08 18:56:39 +0800

    bsdfsdf

--- WordGuessApplication/Form1.cs
+++ WordGuessApplication/Form1.cs
@@ -123,7 +123,7 @@ namespace WordGuessApplication
         
         void RandomQuestion()
         {
-            word = words[rand.Next(0, words.Length - 1)];
+            word = words[rand.Next(0, words.Length - 1)];//YAWA
         }
         string question = null;
         void Setup()
