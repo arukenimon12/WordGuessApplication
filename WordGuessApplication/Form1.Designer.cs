11ea9d97ec1b7caa19ede27049d69628dea83346
Michael John Revilla <revilla.261443@gensan.sti.edu.ph>
2024-04-08 18:52:55 +0800

    Add project files.

---
+++ WordGuessApplication/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace WordGuessApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbGuess = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuess.ForeColor = System.Drawing.Color.White;
+            this.lblGuess.Location = new System.Drawing.Point(148, 57);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(47, 20);
+            this.lblGuess.TabIndex = 0;
+            this.lblGuess.Text = "label";
+            this.lblGuess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(75, 130);
+            this.txtGuess.Multiline = true;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(233, 36);
+            this.txtGuess.TabIndex = 1;
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGuess.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtGuess_KeyDown);
+            this.txtGuess.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtGuess_KeyUp);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnGuess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.Color.White;
+            this.btnGuess.Location = new System.Drawing.Point(121, 172);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(141, 36);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 112);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(425, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Wrong Guess";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(384, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(201, 36);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbGuess
+            // 
+            this.lbGuess.FormattingEnabled = true;
+            this.lbGuess.Location = new System.Drawing.Point(385, 55);
+            this.lbGuess.Name = "lbGuess";
+            this.lbGuess.Size = new System.Drawing.Size(210, 160);
+            this.lbGuess.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 228);
+            this.Controls.Add(this.lbGuess);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblGuess);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess The Word";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox lbGuess;
+    }
+}
---
+++ WordGuessApplication/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordGuessApplication
+{
+    public partial class Form1 : Form
+    {
+
+        private String word = "computer";
+        private Random rand = new Random();
+        private String[] words =
+        {
+            "Abacus",
+            "Abyss",
+            "Achieve",
+            "Acoustic",
+            "Admire",
+            "Adventure",
+            "Affair",
+            "Agriculture",
+            "Airplane",
+            "Alchemist",
+            "Algebra",
+            "Algorithm",
+            "Alligator",
+            "Alphabet",
+            "Amulet",
+            "Anatomy",
+            "Angel",
+            "Anger",
+            "Animal",
+            "Anniversary",
+            "Antenna",
+            "Antique",
+            "Apartment",
+            "Apocalypse",
+            "Apparel",
+            "Appliance",
+            "Aquarium",
+            "Arboretum",
+            "Architecture",
+            "Arena",
+            "Arithmetic",
+            "Armchair",
+            "Aroma",
+            "Artichoke",
+            "Asparagus",
+            "Assemble",
+            "Astronomy",
+            "Athletic",
+            "Atmosphere",
+            "Attic",
+            "Auction",
+            "Audio",
+            "Authentic",
+            "Autograph",
+            "Automobile",
+            "Autumn",
+            "Avalanche",
+            "Avocado",
+            "Award",
+            "Baby",
+            "Backpack",
+            "Bacon",
+            "Baggage",
+            "Bailiff",
+            "Baker",
+            "Balcony",
+            "Ballad",
+            "Ballot",
+            "Bamboo",
+            "Banana",
+            "Bandana",
+            "Bandit",
+            "Banker",
+            "Banquet",
+            "Barber",
+            "Barefoot",
+            "Bargain",
+            "Barnacle",
+            "Barricade",
+            "Baseball",
+            "Baseline",
+            "Basketball",
+            "Bathrobe",
+            "Beach",
+            "Beanbag",
+            "Bear",
+            "Beard",
+            "Beast",
+            "Beauty",
+            "Bedspread",
+            "Beefsteak",
+            "Beeswax",
+            "Beethoven",
+            "Begonia",
+            "Belladonna",
+            "Belligerent",
+            "Beltway",
+            "Benevolent",
+            "Beret",
+            "Bermuda",
+            "Berry",
+            "Beryl",
+            "Beverage",
+            "Bible",
+            "Bicycle",
+            "Bigfoot",
+            "Billboard",
+            "Binocular",
+            "Biography",
+            "Biology"
+        };
+
+        private Timer timer = new Timer();
+        
+        void RandomQuestion()
+        {
+            word = words[rand.Next(0, words.Length - 1)];
+        }
+        string question = null;
+        void Setup()
+        {
+            //RandomQuestion();
+            question = "";
+            for(int i = 0; i < word.Length; i++)
+            {
+
+                if(i!=0 && i!= word.Length - 1)
+                {
+                    question += "?";
+                }
+                else
+                {
+                    question += word.ToCharArray()[i].ToString();
+                }
+
+            }
+            lblGuess.Text = question + "\n" + word;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Setup();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            if (txtGuess.Text.ToLower() != word.ToLower())
+            {
+                lbGuess.Items.Add(txtGuess.Text);
+                MessageBox.Show("Wrong guess!\nTry again.");
+            }
+            else
+            {
+
+                timer.Interval = 3000;
+
+                timer.Tick += (o, v) =>
+                {
+                    lblGuess.Text = question + "\n" + word;
+                    timer.Stop();
+                };
+                lblGuess.Text = word;
+                timer.Start();
+
+                
+                MessageBox.Show("Correct guess!");
+                Setup();
+            }
+
+        }
+        
+        
+        private void txtGuess_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            /*if (e.KeyCode == Keys.D)
+            {
+                test = true;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+
+                fireBullet_ = true;
+            }*/
+
+        }
+
+        private void txtGuess_KeyUp(object sender, KeyEventArgs e)
+        {
+            /*if (e.KeyCode == Keys.D)
+            {
+                test = false;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+
+                fireBullet_ = false;
+            }*/
+        }
+    }
+}
